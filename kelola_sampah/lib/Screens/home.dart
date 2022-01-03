d98be87526dbd338db520c8041912dc365efc9c8
nsenja <nabilasw34@gmail.com>
2022-01-03 14:37:09 +0700

    dashboard home - profile

--- kelola_sampah/lib/Screens/home.dart
+++ kelola_sampah/lib/Screens/home.dart
@@ -1,23 +1,70 @@
+import 'dart:html';
+
 import 'package:flutter/material.dart';
-import 'package:kelola_sampah/components/body.dart';
+import 'package:kelola_sampah/constants.dart';
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+import 'package:kelola_sampah/Screens/dashboard.dart';
+import 'package:kelola_sampah/Screens/myprofile.dart';
 
-   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      appBar: buildAppbar(),
-      body: Body(),
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  //  @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     backgroundColor: Colors.white,
+  //     appBar: buildAppbar(),
+  //     body: Body(),
+  //   );
+  // }
+
+    @override
+  _HomePageState createState() => _HomePageState();
+}
+
+ class _HomePageState extends State<HomePage> {
+  int _selectedIndex = 0;
+  List<Widget> _widgetlist = [Dashboard(), MyProfile()];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  AppBar buildAppbar() {
+    return AppBar(
+      backgroundColor: Colors.teal.shade700,
+      elevation: 0,
     );
   }
 
-   AppBar buildAppbar() {
-     return AppBar(
-      title: Text("Home"),
-       backgroundColor: Colors.teal.shade700,
-       elevation: 0,
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      // appBar: buildAppbar(),
+      body: _widgetlist.elementAt(_selectedIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: kPrimaryColor,
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+        // backgroundColor:,
+      ),
     );
-   }
+  }
 }
---
+++ kelola_sampah/lib/Screens/volunteers.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:kelola_sampah/constants.dart';
+
+class Volunteers extends StatelessWidget {
+  const Volunteers({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+
+    return SafeArea(
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            
+   
+          ],
+        ),
+      ),
+    );
+  }
+}
